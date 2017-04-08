edc38aaf993f6f297aafb26b9ac584b38ad2adba
HellowPixl <HellowPixl@Gmail.com>
2017-04-08 21:12:19 +0200

    New Size

--- desktop/src/fr/tommarx/ohgj/desktop/DesktopLauncher.java
+++ desktop/src/fr/tommarx/ohgj/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import fr.tommarx.ohgj.GameClass;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1248;
+		config.width = 1280;
 		config.height = 720;
 		new LwjglApplication(new GameClass(), config);
 	}
