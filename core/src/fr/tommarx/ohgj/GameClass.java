02b3eef2c7521a57c031ab3273650ef5edc503b3
Tom <tom.marx@hotmail.fr>
2017-04-08 20:58:44 +0200

    Initial commit

---
+++ core/src/fr/tommarx/ohgj/GameClass.java
@@ -0,0 +1,10 @@
+package fr.tommarx.ohgj;
+
+import fr.tommarx.gameengine.Game.Game;
+
+public class GameClass extends Game {
+
+	public void init() {
+
+	}
+}
---
+++ desktop/src/fr/tommarx/ohgj/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package fr.tommarx.ohgj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.tommarx.ohgj.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1248;
+		config.height = 720;
+		new LwjglApplication(new GameClass(), config);
+	}
+}
